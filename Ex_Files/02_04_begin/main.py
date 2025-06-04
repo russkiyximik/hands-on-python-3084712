9f9a347b58b66848435c5d8971548dacbad40c32
russkiyximik <alex.ponomar37@gmail.com>
2025-06-04 00:48:12 +0000

    done

--- Ex_Files/02_04_begin/main.py
+++ Ex_Files/02_04_begin/main.py
@@ -1,6 +1,6 @@
-NAMES = ["John", "Paul", "George", "Ringo"]
-AGES = [20, 21, 22, 23]
-
+NAMES = ["John", "Paul", "George", "Ringo", "Alex", "Joe Bob"]
+AGES = [20, 21, 22, 23, 18, 45]
+'''
 i = 0
 while i < len(NAMES):
     print(NAMES[i], AGES[i])
@@ -8,14 +8,17 @@ while i < len(NAMES):
 
 for name in NAMES:
     print(name)
-
+'''
 for name, age in zip(NAMES, AGES):
-    print(f"{name} {age}")
-
+    print(f"{name} is {age} years old.")
+'''
 for name in reversed(NAMES):
     print(name)
 
 for i in range(5):
     print(i)
-
+'''
 # enumerate
+
+for i, name in enumerate(NAMES):
+    print(f"{i}: {name}, {age}")
